40c0e4ee878e88d2f83d4388c38a6979cc71cf1f
shifa <shifa.karnelia@georgebrown.ca>
2025-12-04 19:05:37 -0500

    add group member info

--- A2_shifa_karnelia/src/screens/AboutScreen.js
+++ A2_shifa_karnelia/src/screens/AboutScreen.js
@@ -3,9 +3,12 @@ import { View, Text, StyleSheet } from 'react-native';
 const AboutScreen = () => {
      return (
     <View >
+        <Text>Group 15</Text>
       <Text >Name:- shifa karnleia</Text>
       <Text >Student ID: 101487402</Text>
-      </View>
+      <Text >Name:- sana karnleia</Text>
+      <Text >Student ID: 101487075</Text>
+    </View>
      );
 }
 
