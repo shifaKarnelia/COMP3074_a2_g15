ff574cb42f9965ba1a812d9dc560711b484db3f9
shifa <shifa.karnelia@georgebrown.ca>
2025-12-04 19:10:43 -0500

    add style onabputscreen

--- A2_shifa_karnelia/src/screens/AboutScreen.js
+++ A2_shifa_karnelia/src/screens/AboutScreen.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet} from 'react-native';
 const AboutScreen = () => {
      return (
-    <View >
-        <Text>Group 15</Text>
-      <Text >Name:- shifa karnleia</Text>
-      <Text >Student ID: 101487402</Text>
-      <Text >Name:- sana karnleia</Text>
-      <Text >Student ID: 101487075</Text>
+    <View style={styles.container} >
+        <Text style={styles.header}>Group 15</Text>
+      <Text style={styles.name}>Name:- shifa karnleia</Text>
+      <Text style={styles.studentId}>Student ID: 101487402</Text>
+      <Text style={styles.name}>Name:- sana karnleia</Text>
+      <Text style={styles.studentId}>Student ID: 101487075</Text>
     </View>
      );
+
+     
 }
+const styles = StyleSheet.create({
+  container: {
+            flex: 1,
+            padding: 16,
+            backgroundColor: '#fff',
+        },
+     header: {
+    fontSize: 28,
+    fontWeight: '800',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+    name: {
+            fontSize: 22,
+            fontWeight: '700',
+            marginBottom: 4,
+        },
+    studentId: {
+            fontSize: 16,
+            marginBottom: 16,
+        },
+});
 
 export default AboutScreen;
