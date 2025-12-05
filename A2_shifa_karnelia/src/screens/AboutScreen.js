b3108563ffeb9387a5b47827044991ce1689c7d2
shifa <shifa.karnelia@georgebrown.ca>
2025-12-04 19:04:07 -0500

    add personal info into aboutscreen

--- A2_shifa_karnelia/src/screens/AboutScreen.js
+++ A2_shifa_karnelia/src/screens/AboutScreen.js
@@ -1,4 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 const AboutScreen = () => {
+     return (
+    <View >
+      <Text >Name:- shifa karnleia</Text>
+      <Text >Student ID: 101487402</Text>
+      </View>
+     );
 }
 
 export default AboutScreen;
